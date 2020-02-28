c792b5a0387f22ab0d8fe781dba9402bb690cc3d
JoaoLucas <joao.santos@hivolve.com>
2020-02-28 16:33:22 +0000

    ARCore Base

---
+++ Assets/Scripts/AR/ArManager.cs
@@ -0,0 +1,39 @@
+﻿using AR.ARCore;
+using Assets.Scripts.Common;
+using UnityEngine;
+
+namespace AR
+{
+    public class ArManager : Singleton<ArManager>
+    {
+        [Header("AR Components")]
+        public GameObject arKitSection;
+        public GameObject arCoreSection;
+
+        [Header("Object placement components")]
+        public ArCoreObjectPlacementManipulator arCorePlacementManipulator;
+
+        protected override void Awake()
+        {
+#if UNITY_ANDROID
+            arKitSection.SetActive(false);
+            arCoreSection.SetActive(true);
+#elif UNITY_IOS
+        arKitSection.SetActive(true);
+        arCoreSection.SetActive(false);
+#endif
+        }
+
+
+        public void SetFloorScanningObject(GameObject prefab)
+        {
+#if UNITY_ANDROID
+            arCorePlacementManipulator.prefab = prefab;
+#elif UNITY_IOS
+        arKitSection.SetActive(true);
+        arCoreSection.SetActive(false);
+#endif
+        }
+
+    }
+}
