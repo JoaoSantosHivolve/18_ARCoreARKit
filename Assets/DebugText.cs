86a2831b35f41e51ca06fdcdb03770953a63419b
JoaoLucas <joao.santos@hivolve.com>
2020-03-06 12:27:53 +0000

    ArKit Scaling Test 13

---
+++ Assets/DebugText.cs
@@ -0,0 +1,10 @@
+﻿using Assets.Scripts.Common;
+using TMPro;
+
+public class DebugText : Singleton<DebugText>
+{
+    public string Text
+    {
+        set => GetComponent<TextMeshProUGUI>().text = value;
+    }
+}
--- Assets/Scripts/AR/ARKit/Manipulators/ArKitScalingManipulator.cs
+++ Assets/Scripts/AR/ARKit/Manipulators/ArKitScalingManipulator.cs
@@ -1,4 +1,5 @@
 ﻿using Common;
+using TMPro;
 using UnityEngine;
 
 namespace AR.ARKit.Manipulators
@@ -12,6 +13,9 @@ namespace AR.ARKit.Manipulators
         public float maxSize;
         [Range(-0.50f, -0.01f)]
         public float minSize;
+
+        private float m_MinPinchDistance = 1;
+        private float m_MaxPinchDistance = 100;
 
         private float m_PinchDistanceDelta;
 
@@ -47,35 +51,56 @@ namespace AR.ARKit.Manipulators
             arKitObject.transform.localScale = Vector3.one + (Vector3.one * PinchDistanceDelta);
         }
 
+        private Vector2 firstTouchPos1;
+        private Vector2 firstTouchPos2;
+
         private void Calculate()
         {
-            if (Input.touchCount != 2)
+            if (Input.touchCount == 1)
             {
-                isScaling = false;
-                return;
+                if(Input.GetTouch(0).phase == TouchPhase.Began)
+                    firstTouchPos1 = Input.GetTouch(0).position;
             }
 
-            var touch1 = Input.touches[0];
-            var touch2 = Input.touches[1];
-
-            // ... if at least one of them moved ...
-            if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
+            if (Input.touchCount == 2)
             {
-                // ... check the delta distance between them ...
-                m_PinchDistance = Vector2.Distance(touch1.position, touch2.position);
+                if (Input.GetTouch(1).phase == TouchPhase.Began)
+                    firstTouchPos2 = Input.GetTouch(1).position;
 
-                var prevDistance = Vector2.Distance(touch1.position - touch1.deltaPosition, touch2.position - touch2.deltaPosition);
+                var touch1 = Input.GetTouch(0);
+                var touch2 = Input.GetTouch(1);
 
-                var distance = m_PinchDistance - prevDistance;
-
-                // ... if it's greater than a minimum threshold, it's a pinch!
-                if (Mathf.Abs(distance) > MinPinchDistance)
+                var scaleValue = 0.0f;
+                if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
                 {
-                    isScaling = true;
+                    scaleValue += Vector2.Distance(firstTouchPos1, touch1.position);
+                    scaleValue += Vector2.Distance(firstTouchPos2, touch2.position);
 
-                    PinchDistanceDelta = Helper.RemapNumber(distance - MinPinchDistance, 0, 50, minSize, maxSize);
+                    DebugText.Instance.Text = scaleValue.ToString();
+
+                    if (scaleValue > m_MinPinchDistance)
+                    {
+                        isScaling = true;
+                        PinchDistanceDelta = Helper.RemapNumber(scaleValue, 0, m_MaxPinchDistance, minSize, maxSize);
+                    }
+
+                    //// ... check the delta distance between them ...
+                    //m_PinchDistance = Vector2.Distance(touch1.position, touch2.position);
+                    //
+                    //var prevDistance = Vector2.Distance(touch1.position - touch1.deltaPosition, touch2.position - touch2.deltaPosition);
+                    //
+                    //var distance = m_PinchDistance - prevDistance;
+                    //
+                    //// ... if it's greater than a minimum threshold, it's a pinch!
+                    //if (Mathf.Abs(distance) > MinPinchDistance)
+                    //{
+                    //    isScaling = true;
+                    //
+                    //    PinchDistanceDelta = Helper.RemapNumber(distance - MinPinchDistance, 0, 50, minSize, maxSize);
+                    //}
                 }
             }
+            else isScaling = false;
         }
     }
 }
